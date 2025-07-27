aece27217d1e29aaff10bbce9a51470a4d25b2ea
freelancerhaze <151395995+freelancerhaze@users.noreply.github.com>
2025-07-27 06:28:48 +0000

    Some refactors

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,27 +1,19 @@
 -- Standalone plugins with less than 10 lines of config go here
 return {
-  {
-    -- Powerful Git integration for Vim
-    'tpope/vim-fugitive',
-  },
-  {
-    -- GitHub integration for vim-fugitive
-    'tpope/vim-rhubarb',
-  },
-  
-  {
-    -- Autoclose parentheses, brackets, quotes, etc.
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = true,
-    opts = {},
-  },
-  
-  {
-    -- High-performance color highlighter
-    'norcalli/nvim-colorizer.lua',
-    config = function()
-      require('colorizer').setup()
-    end,
-  },
+	{
+		-- Powerful Git integration for Vim
+		"tpope/vim-fugitive",
+	},
+	{
+		-- GitHub integration for vim-fugitive
+		"tpope/vim-rhubarb",
+	},
+
+	{
+		-- Autoclose parentheses, brackets, quotes, etc.
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		opts = {},
+	},
 }
