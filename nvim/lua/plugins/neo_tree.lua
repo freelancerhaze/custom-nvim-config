8e5fbd69621f323cb111fd9c74ea5e53ea940fab
freelancerhaze <151395995+freelancerhaze@users.noreply.github.com>
2025-08-02 09:35:58 +0000

    Added catppuccin.lua also modified Neotree configurations a bit

--- nvim/lua/plugins/neo_tree.lua
+++ nvim/lua/plugins/neo_tree.lua
@@ -1,21 +1,43 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	lazy = false,
+	opts = {
+		close_if_last_window = true,
+		popup_border_style = "rounded",
+		enable_git_status = true,
+		enable_diagnostics = false,
+		filesystem = {
+			follow_current_file = {
+				enabled = false, -- No auto-expansion
+			},
+			hijack_netrw_behavior = "disabled", -- Don't auto-open Neo-tree
+			filtered_items = {
+				hide_dotfiles = false,
+				hide_gitignored = false,
+			},
+			window = {
+				position = "right",
+				width = 30,
+				mappings = {
+					["<space>"] = "toggle_node", -- Manual expand/collapse
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		require("neo-tree").setup(opts)
 
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- Optional image support for file preview: See `# Preview Mode` for more information.
-    -- {"3rd/image.nvim", opts = {}},
-    -- OR use snacks.nvim's image module:
-    -- "folke/snacks.nvim",
-  },
-  
-  lazy = false, -- neo-tree will lazily load itself
-  ---@module "neo-tree"
-  ---@type neotree.Config?
-  opts = {
-    -- add options here
-  }
+		-- Toggle Neo-tree with <leader>e
+		vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {
+			desc = "Toggle Neo-tree",
+			noremap = true,
+			silent = true,
+		})
+	end,
 }
